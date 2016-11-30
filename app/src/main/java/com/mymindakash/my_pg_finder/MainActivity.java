649d0c79a5ed05a651d64e60d453581c5a1170c8
Akash Vishnu Sangle <akashsangle377@gmail.com>
2016-11-30 11:22:57 +0530

    Initial commit

---
+++ app/src/main/java/com/mymindakash/my_pg_finder/MainActivity.java
@@ -0,0 +1,13 @@
+package com.mymindakash.my_pg_finder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
