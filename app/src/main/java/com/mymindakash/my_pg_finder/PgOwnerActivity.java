b703aeb641f500a68e8da79ad2cf2b9554bad143
vatsal shingala <shingalavatsal@gmail.com>
2016-11-30 12:42:33 +0530

    Toast message
    see the toast message

--- app/src/main/java/com/mymindakash/my_pg_finder/PgOwnerActivity.java
+++ app/src/main/java/com/mymindakash/my_pg_finder/PgOwnerActivity.java
@@ -1,8 +1,23 @@
 package com.mymindakash.my_pg_finder;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
 /**
  * Created by vatsal on 11/30/2016.
  */
 
-public class PgOwnerActivity {
+public class PgOwnerActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Toast.makeText(getApplicationContext(),"hello world from the app",Toast.LENGTH_LONG).show();
+    }
+
+
 }
