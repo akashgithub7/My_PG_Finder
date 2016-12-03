30ee7741f50dd2b3d786676e925485c0209b8606
Vikram Ahir <ahir345@gmail.com>
2016-12-03 23:33:50 +0530

    create some validation in login form..

--- app/src/main/java/com/mymindakash/my_pg_finder/fragments/User_Login_Form.java
+++ app/src/main/java/com/mymindakash/my_pg_finder/fragments/User_Login_Form.java
@@ -3,9 +3,13 @@ package com.mymindakash.my_pg_finder.fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.mymindakash.my_pg_finder.R;
 
@@ -14,10 +18,34 @@ import com.mymindakash.my_pg_finder.R;
  */
 
 public class User_Login_Form extends Fragment {
+    private EditText etLoginEmail,getEtLoginPassword;
+    private Button btnLogin;
 
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.user_login_form,container,false);
+        View view= inflater.inflate(R.layout.user_login_form,container,false);
+        etLoginEmail= (EditText) view.findViewById(R.id.etLoginEmail);
+        getEtLoginPassword= (EditText) view.findViewById(R.id.etLoginPassword);
+        btnLogin= (Button) view.findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              if(etLoginEmail.getText().toString().equals("")  ){
+                  etLoginEmail.setError("error");
+              }else if ( getEtLoginPassword.getText().toString().equals("")){
+                  getEtLoginPassword.setError("error");
+              }else {
+                  etLoginEmail.setText("");
+                  getEtLoginPassword.setText("");
+              }
+            }
+        });
+        return view;
     }
+
+
 }
