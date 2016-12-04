7b5008294523f9a75c823d4c6d2600562ce96856
Vikram Ahir <ahir345@gmail.com>
2016-12-04 17:32:48 +0530

    Make validation in  User Register   form..

--- app/src/main/java/com/mymindakash/my_pg_finder/fragments/User_Register_Form.java
+++ app/src/main/java/com/mymindakash/my_pg_finder/fragments/User_Register_Form.java
@@ -1,16 +1,18 @@
 package com.mymindakash.my_pg_finder.fragments;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
+import android.widget.Button;
+import android.widget.EditText;
+
 
 import com.mymindakash.my_pg_finder.R;
+
 
 /**
  * Created by Vikram on 12/3/2016.
@@ -18,10 +20,39 @@ import com.mymindakash.my_pg_finder.R;
 
 public class User_Register_Form extends Fragment {
 
+    private EditText etName,etNumber,etPassword,etEmail;
+    private Button btnRegister;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.user_register_form,container,false);
+       View view=inflater.inflate(R.layout.user_register_form,container,false);
+        etName= (EditText) view.findViewById(R.id.etName);
+        etNumber= (EditText) view.findViewById(R.id.etNumber);
+        etEmail= (EditText) view.findViewById(R.id.etEmail);
+        etPassword= (EditText) view.findViewById(R.id.etPassword);
+        btnRegister= (Button) view.findViewById(R.id.btnRegister);
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(etName.getText().toString().equals("")){
+                    etName.setError("error");
+                }else if(etNumber.getText().toString().equals("")){
+                    etNumber.setError("error");
+                }else  if(etEmail.getText().toString().equals("")){
+                    etEmail.setError("error");
+                }else  if(etPassword.getText().toString().equals("")){
+                    etPassword.setError("error");
+                }else {
+                    etName.setText("");
+                    etNumber.setText("");
+                    etEmail.setText("");
+                    etPassword.setText("");
+
+                }
+            }
+        });
+        return view;
     }
 
 }
---
+++ app/src/main/java/com/mymindakash/my_pg_finder/mailsend/MailConfiguration.java
@@ -0,0 +1,11 @@
+package com.mymindakash.my_pg_finder.mailsend;
+
+/**
+ * Created by Vikram on 12/4/2016.
+ */
+
+public class MailConfiguration {
+
+    public static final String EMAIL ="ahir345@gmail.com";
+    public static final String PASSWORD ="8238823001";
+}
