b8af55ea8f033a8420ce22e65fb5363f0521f28e
Akash Vishnu Sangle <akashsangle377@gmail.com>
2016-12-05 10:28:09 +0530

    Some Changes in Splash Screen Acivity

--- app/src/main/java/com/mymindakash/my_pg_finder/SplashActivity.java
+++ app/src/main/java/com/mymindakash/my_pg_finder/SplashActivity.java
@@ -21,19 +21,19 @@ import com.mymindakash.my_pg_finder.KenBurnsView;
 public class SplashActivity extends Activity {
 
     // Splash screen timer
-    private static int SPLASH_TIME_OUT = 20000;
+    private static int SPLASH_TIME_OUT = 10000;
     private KenBurnsView mKenBurns;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        setContentView(R.layout.splash_layout);
         setAnimation();
 
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
 
         mKenBurns = (KenBurnsView) findViewById(R.id.ken_burns_images);
-        mKenBurns.setImageResource(R.drawable.window_wall);
+        mKenBurns.setImageResource(R.drawable.windows);
 
         new Handler().postDelayed(new Runnable() {
 
@@ -50,13 +50,13 @@ public class SplashActivity extends Activity {
     private void setAnimation() {
         ObjectAnimator scaleXAnimation = ObjectAnimator.ofFloat(findViewById(R.id.welcome_text), "scaleX", 5.0F, 1.0F);
         scaleXAnimation.setInterpolator(new AccelerateDecelerateInterpolator());
-        scaleXAnimation.setDuration(1200);
+        scaleXAnimation.setDuration(500);
         ObjectAnimator scaleYAnimation = ObjectAnimator.ofFloat(findViewById(R.id.welcome_text), "scaleY", 5.0F, 1.0F);
         scaleYAnimation.setInterpolator(new AccelerateDecelerateInterpolator());
-        scaleYAnimation.setDuration(1200);
+        scaleYAnimation.setDuration(500);
         ObjectAnimator alphaAnimation = ObjectAnimator.ofFloat(findViewById(R.id.welcome_text), "alpha", 0.0F, 1.0F);
         alphaAnimation.setInterpolator(new AccelerateDecelerateInterpolator());
-        alphaAnimation.setDuration(1200);
+        alphaAnimation.setDuration(500);
         AnimatorSet animatorSet = new AnimatorSet();
         animatorSet.play(scaleXAnimation).with(scaleYAnimation).with(alphaAnimation);
         animatorSet.setStartDelay(500);
