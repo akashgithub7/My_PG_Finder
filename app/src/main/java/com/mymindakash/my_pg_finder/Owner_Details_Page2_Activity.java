04945057c470c29956b49a8052ae0e751805ba0e
Akash Vishnu Sangle <akashsangle377@gmail.com>
2016-12-08 11:38:58 +0530

    Added owner page 2 UI

---
+++ app/src/main/java/com/mymindakash/my_pg_finder/Owner_Details_Page2_Activity.java
@@ -0,0 +1,18 @@
+package com.mymindakash.my_pg_finder;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Akash117759 on 08-12-2016.
+ */
+
+public class Owner_Details_Page2_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.owner_details_page2_layout);
+    }
+}
